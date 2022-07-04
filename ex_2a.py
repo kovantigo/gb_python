7c804efcf87a5cca4f10ad05597dfeda253fe845
kovantigo <56314498+kovantigo@users.noreply.github.com>
2022-07-04 20:50:18 +0300

    Update ex_2a.py

--- ex_2a.py
+++ ex_2a.py
@@ -1,14 +1,23 @@
+'''
+2. Создать список, состоящий из кубов нечётных чисел от 1 до 1000 (куб X - третья степень числа X):
+Вычислить сумму тех чисел из этого списка, сумма цифр которых делится нацело на 7. 
+Например, число «19 ^ 3 = 6859» будем включать в сумму, так как 6 + 8 + 5 + 9 = 28 – делится нацело на 7. 
+Внимание: использовать только арифметические операции!
+'''
+
+def sum_of_digits(number):
+    sum_digits = 0
+    while number > 0:
+        sum_digits += (number % 10)
+        number //= 10
+    return sum_digits
+
 cube_numbers = []
-for number in range(1,1001,2):
+for number in range(1, 1001, 2):
     cube_numbers.append(number ** 3)
 
 sum_numbers = 0
-for index in range(len(cube_numbers)):
-    sum_digits = 0
-    curr_number = cube_numbers[index]
-    while curr_number > 0:
-        sum_digits += curr_number % 10
-        curr_number //= 10
-    if sum_digits % 7 == 0:
-        sum_numbers += cube_numbers[index]
+for x in cube_numbers:
+    if sum_of_digits(x) % 7 == 0:
+        sum_numbers += x
 print(sum_numbers)
